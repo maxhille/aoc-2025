9ab9970d05a3af6e431076bf6a23e558fcc88681
Max Hille <mh@lambdasoup.com>
2025-12-02 11:24:03 +0100

    Day 2 - Part 2

--- days/Day02.elm
+++ days/Day02.elm
@@ -1,4 +1,4 @@
-module Day02 exposing (invalids, parser, part1, puzzle)
+module Day02 exposing (invalids, isRepeated1, isRepeated2, parser, part1, part2, puzzle)
 
 import Parser exposing ((|.), (|=), Parser, Trailing(..))
 import Puzzle exposing (Puzzle, Step(..))
@@ -14,43 +14,58 @@ type alias State =
     }
 
 
+view : State -> List String
+view { ranges, invalidIds } =
+    [ "invalidIds: " ++ (invalidIds |> List.length |> String.fromInt)
+    , "ranges to go: " ++ (ranges |> List.length |> String.fromInt)
+    ]
+
+
 part1 : Puzzle.Part
 part1 =
     Puzzle.part
-        { view =
-            \{ ranges, invalidIds } ->
-                [ "invalidIds: " ++ (invalidIds |> List.map String.fromInt |> String.join " ")
-                , "ranges to go: " ++ (ranges |> List.length |> String.fromInt)
-                ]
+        { view = view
         , result = .invalidIds >> List.sum >> String.fromInt
         , parser = parser
         , init = \ranges -> { ranges = ranges, invalidIds = [] }
-        , step = countInvalids
+        , step = countInvalids isRepeated1
         }
 
 
-countInvalids : State -> Step State
-countInvalids { ranges, invalidIds } =
+part2 : Puzzle.Part
+part2 =
+    Puzzle.part
+        { view = view
+        , result = .invalidIds >> List.sum >> String.fromInt
+        , parser = parser
+        , init = \ranges -> { ranges = ranges, invalidIds = [] }
+        , step = countInvalids isRepeated2
+        }
+
+
+countInvalids : (Int -> Bool) -> State -> Step State
+countInvalids isRepeated { ranges, invalidIds } =
     case ranges of
         range :: rest ->
-            Loop { ranges = rest, invalidIds = invalidIds ++ invalids range }
+            Loop { ranges = rest, invalidIds = invalidIds ++ invalids isRepeated range }
 
         [] ->
             Done { ranges = ranges, invalidIds = invalidIds }
 
 
-invalids : Range -> List Int
-invalids ( id1, id2 ) =
-    invalidsHelp [] id1 id2
+invalids : (Int -> Bool) -> Range -> List Int
+invalids isRepeated ( id1, id2 ) =
+    invalidsHelp isRepeated [] id1 id2
 
 
-invalidsHelp : List Int -> Int -> Int -> List Int
-invalidsHelp invalidIds lower upper =
+invalidsHelp : (Int -> Bool) -> List Int -> Int -> Int -> List Int
+invalidsHelp isRepeated invalidIds lower upper =
     if lower > upper then
         invalidIds
 
     else
         invalidsHelp
+            isRepeated
             (invalidIds
                 ++ (if isRepeated lower then
                         [ lower ]
@@ -63,8 +78,8 @@ invalidsHelp invalidIds lower upper =
             upper
 
 
-isRepeated : Int -> Bool
-isRepeated int =
+isRepeated1 : Int -> Bool
+isRepeated1 int =
     let
         str =
             String.fromInt int
@@ -78,6 +93,43 @@ isRepeated int =
                 String.length str // 2
         in
         String.left len str == String.right len str
+
+
+isRepeated2 : Int -> Bool
+isRepeated2 int =
+    isRepeated2Help 1 (String.fromInt int)
+
+
+isRepeated2Help : Int -> String -> Bool
+isRepeated2Help digits id =
+    let
+        subId =
+            String.left digits id
+    in
+    if digits > String.length id // 2 then
+        False
+
+    else if repeats subId id then
+        True
+
+    else
+        isRepeated2Help (digits + 1) id
+
+
+repeats : String -> String -> Bool
+repeats subId id =
+    let
+        len =
+            String.length subId
+    in
+    if String.left len id == subId then
+        repeats subId (String.dropLeft len id)
+
+    else if id == "" then
+        True
+
+    else
+        False
 
 
 parser : Parser (List Range)
@@ -98,4 +150,4 @@ parser =
 
 puzzle : Puzzle
 puzzle =
-    { parts = [ part1 ] }
+    { parts = [ part1, part2 ] }
--- tests/Day02Test.elm
+++ tests/Day02Test.elm
@@ -23,6 +23,18 @@ suite =
                             """
                 in
                 Expect.equal "1227775554" (example |> compute part1)
+        , test "Part 2 - Example" <|
+            \_ ->
+                let
+                    example =
+                        sanitize <|
+                            """
+                            11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+                            1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+                            824824821-824824827,2121212118-2121212124
+                            """
+                in
+                Expect.equal "4174379265" (example |> compute part2)
         , test "parser" <|
             \_ ->
                 let
@@ -47,6 +59,24 @@ suite =
             "Part 1 - Invalids"
             [ test "11-22" <|
                 \_ ->
-                    Expect.equal [ 11, 22 ] (invalids ( 11, 22 ))
+                    Expect.equal [ 11, 22 ] (invalids isRepeated1 ( 11, 22 ))
+            ]
+        , describe
+            "Part 2 - Invalids"
+            [ test "11-22" <|
+                \_ ->
+                    Expect.equal [ 11, 22 ] (invalids isRepeated2 ( 11, 22 ))
+            ]
+        , describe
+            "Part 2 - isRepeated2"
+            [ test "12341234" <|
+                \_ ->
+                    Expect.equal True (isRepeated2 12341234)
+            , test "1212121212" <|
+                \_ ->
+                    Expect.equal True (isRepeated2 1212121212)
+            , test "12" <|
+                \_ ->
+                    Expect.equal False (isRepeated2 12)
             ]
         ]
